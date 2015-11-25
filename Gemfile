6535284ddbd4bbf6fdd814524496e34785ed8f81
Hoang The Toan <toanht15@gmail.com>
2015-11-25 15:50:17 +0700

    resf #46187 sign up

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source "https://rubygems.org"
 
 gem "rails", "4.2.4"
+gem "bcrypt", "3.1.7"
 gem "rails-i18n", "~> 4.0.0"
 gem "bootstrap-sass", "3.2.0.0"
 gem "sqlite3"
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,10 +2,19 @@ class User < ActiveRecord::Base
   has_many :activities
   has_many :lessons
   has_many :categories, through: :lessons
+  has_secure_password
 
   validates :name,  presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
     format: {with: VALID_EMAIL_REGEX},
     uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}
+
+  before_save :downcase_email
+
+  private
+  def downcase_email
+    self.email = email.downcase
+  end
 end
