2f24939847dce461eee3e6cbaa21c3b2c6a4dd82
Hoang The Toan <toanht15@gmail.com>
2015-11-25 15:50:17 +0700

    resf #46187 sign up

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125062636) do
+ActiveRecord::Schema.define(version: 20151125081542) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "activity"
@@ -68,8 +68,9 @@ ActiveRecord::Schema.define(version: 20151125062636) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
   create_table "words", force: :cascade do |t|
