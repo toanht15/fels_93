07ca7f9d8f2ad75f38a92d4704dc6ca77b008972
Hoang The Toan <toanht15@gmail.com>
2015-11-26 10:48:19 +0700

    resf #46564 user profile

---
+++ app/helpers/sessions_helper.rb
@@ -0,0 +1,18 @@
+module SessionsHelper
+  def log_in user
+    session[:user_id] = user.id
+  end
+
+  def current_user? user
+    user == current_user
+  end
+
+  def redirect_back_or default
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151125081542) do
+ActiveRecord::Schema.define(version: 20151126031423) do
 
   create_table "activities", force: :cascade do |t|
     t.string   "activity"
@@ -68,9 +68,10 @@ ActiveRecord::Schema.define(version: 20151125081542) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at",      null: false
-    t.datetime "updated_at",      null: false
+    t.datetime "created_at",                      null: false
+    t.datetime "updated_at",                      null: false
     t.string   "password_digest"
+    t.boolean  "admin",           default: false
   end
 
   create_table "words", force: :cascade do |t|
