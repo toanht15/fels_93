bd6448b36e66f37ba88b5c6d6fd5ab1e9ef87448
Thuan123 <thuanbk56@gmail.com>
2015-12-01 15:51:46 +0700

    refs#47205-categories

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   get "login" => "sessions#new"
   post "login" => "sessions#create"
   delete "logout"  => "sessions#destroy"
-  resources :users
+  resources :users, except: :destroy
+  resources :categories, only: [:index]
 end
